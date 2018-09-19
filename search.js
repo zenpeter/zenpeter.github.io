570b0c186a1d5440d813c1f9f987b4b550e8049d
zenpeter <peterzenkovich@yandex.ru>
2018-09-19 22:59:56 +0300

    add datepicker

--- search.js
+++ search.js
@@ -1,4 +1,6 @@
-$(function() {
-  //задание заполнителя с помощью параметра placeholder
-  $("#date").mask("99.99.9999", {placeholder: "дд.мм.гггг" });
-});
+if ($(window).width() < 768) {
+  $("#datepickerContainer").append('<input class="datepicker-here" type="date" name="when" required>');
+}
+else {
+  $("#datepickerContainer").append('<input class="datepicker-here" type="text" name="when" required>');
+}
