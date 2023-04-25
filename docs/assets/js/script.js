c257e20f3ab819408da5b327c133a9a6e0518bd0
Zenpeter <peterzenkovich@yandex.ru>
2023-04-25 06:58:28 +0300

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,3 +1,15 @@
+document.querySelector('#projects-button').addEventListener('click', function() {
+  document.querySelector('#projects-section').scrollIntoView({
+    behavior: 'smooth'
+  });
+});
+
+document.querySelector('#connect-button').addEventListener('click', function() {
+  document.querySelector('#footer-section').scrollIntoView({
+    behavior: 'smooth'
+  });
+});
+
 mybutton = document.getElementById("footer__button-up");
 function topFunction() {
   document.body.scrollTop = 0;
